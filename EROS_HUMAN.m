ecd9ac5dccc8a906036d762bc1632800811c2caa
VlastaKoudelka <vlasta.koudelka@gmail.com>
2015-09-11 16:40:14 +0200

    working version in serial mode

--- EROS_HUMAN.m
+++ EROS_HUMAN.m
@@ -8,29 +8,33 @@ function EROS_HUMAN
 %                 | (____/\| ) \ \__| (___) |/\____) |
 %                 (_______/|/   \__/(_______)\_______)
 %                                   
-%  modified> 10.9.2015                         coded by> Vlastimil Koudelka
+%  modified> 11.9.2015                         coded by> Vlastimil Koudelka
 %                                       used code by>Robert Glenn Stockwell
 % 
 
 %% Batch execution
 
 close all
-[f_names,f_path] = uigetfile('*.edf','Open the source file','MultiSelect', 'on');
+[f_name,f_path] = uigetfile('*.edf','Open the source file','MultiSelect', 'on');
 tic
 
-if ~(iscell(f_names))                     %in the case of one file
-    f_names = {f_names};
+if ~(iscell(f_name))                     %in the case of one file
+    f_name = {f_name};
 end
 
-subject = read_data(f_path,f_names);
+subject = read_data(f_path,f_name);
 
 for i = 1:length(subject)   %over all subject
-    for j = 1:length(subject.labels)
+    for j = 1:length(subject(i).labels)
         [subject(i).ERO{1,j},subject(i).ERO{2,j},subject(i).ERP{1,j}, ...
          subject(i).ERP{2,j},subject(i).PLI{1,j},subject(i).PLI{2,j}, ...
-         subject(i).f,subject(i).t] = EROS_CALC(subject(i).raw_data{i},subject.triggers);
+         subject(i).f,subject(i).t] = EROS_CALC(subject(i).raw_data{j},subject(i).triggers);
     end
 end
+
+% subject = par_comp(subject);
+
+subject(end + 1) = crt_mean_sbj(subject);
 
 toc
 visualize_eros(subject);
@@ -53,12 +57,16 @@ n_delay = round(delay*Fs);              % #samples of delay
 N = n_pre + n_post + 1;                 % #samples within a trial
 
 %% Prefiltering & Down-sampling
-load filters.mat Num                    %the same anti-aliasing filter for:
+load filter_DP_100_125.mat Num                    %the same anti-aliasing filter for:
 
                                         %Fs=1kHz, fp=100Hz, fs=125Hz
                                         
 data = filtfilt(Num,1,data);            %Zero phase filtering
 data = downsample(data,4)';              %Fs 1kHz -> 250Hz
+
+load filter_HP_0_2.mat Num
+
+data = filtfilt(Num,1,data);            %high pass filter
 
 flags(:,2) = round(flags(:,2)/4);
 
@@ -123,24 +131,52 @@ end
 A_rel_pow = A_mean.^2/max(max(A_mean.^2));    %Relative spectral pow.
 B_rel_pow = B_mean.^2/max(max(B_mean.^2));
 
-t = (t * (t_pre + t_post) - t_pre)*1e3;       %Time axis
+t = (t - t_pre)*1e3;       %Time axis
 
 end
+
+%% Parallel computation
+% function subject = par_comp(subject);
+% 
+% k = 1;
+% for i = 1:length(subject)   %over all subject
+%     for j = 1:length(subject(i).labels)
+%         data{k} = subject(i).raw_data{j};
+%         flags{k} = subject(i).triggers;
+%         k = k + 1;
+%     end
+% end
+% 
+% parfor i = 1:length(data)
+%     
+% end
+% 
+% for i = 1:length(subject)   %over all subject
+%     for j = 1:length(subject(i).labels)
+%         [subject(i).ERO{1,j},subject(i).ERO{2,j},subject(i).ERP{1,j}, ...
+%          subject(i).ERP{2,j},subject(i).PLI{1,j},subject(i).PLI{2,j}, ...
+%          subject(i).f,subject(i).t] = EROS_CALC(subject(i).raw_data{j},subject(i).triggers);
+%     end
+% end
+% end
 
 %% Data loading
 function subject = read_data(path,names)
 
-for i = 1:length(names)                 %over all files
+for i = 1:length(names)                                     %over all files
     [data,header] = ReadEDF(fullfile(path,names{i}));       %read EDF 
+    subject(i).f_name = names{i};
+    subject(i).f_path = path;
     if i == 1    
         [el_idx,ok] = listdlg('PromptString','Select electrodes:','ListString',header.labels);   %select electrodes
-        if length(el_idx) > 2
-            el_idx = el_idx(1:2);
+        if length(el_idx) > 10
+            el_idx = el_idx(1:10);
         end
         
         event = unique(header.annotation.event);
         trig_idx = listdlg('PromptString','Select triggers:','ListString',event);   %select triggers
     end
+    subject(i).n_ch = length(el_idx);
     
     for j = 1:length(el_idx)
         subject(i).raw_data{j} = data{el_idx(j)};
@@ -150,13 +186,12 @@ for i = 1:length(names)                 %over all files
     for j = 1:length(trig_idx)
         subject(i).trig_label{j} = event{trig_idx(j)};
     end
-    subject(i).filename = fullfile(path,names{i});
     
     m = 1;
     for k = 1:length(header.annotation.event)
        for j = 1:length(subject(i).trig_label)
             if strcmp(subject(i).trig_label{j},header.annotation.event{k})
-                subject(i).triggers(m,1) = j;                     %event type
+                subject(i).triggers(m,1) = j;              %event type
                 subject(i).triggers(m,2) = header.annotation.starttime(k);
                 m = m + 1;
             end
@@ -167,34 +202,81 @@ for i = 1:length(names)                 %over all files
 end
 end
 
+%% Average
+function mean_sbj = crt_mean_sbj(subject)
+
+n_sbj = length(subject);
+n_evt = length(subject(1).trig_label);
+n_ch = subject(1).n_ch;
+
+for i = 1:n_evt
+    for j = 1:n_ch    
+        for k = 2:n_sbj             %first subj is cummulative
+            subject(1).ERO{i,j} = subject(1).ERO{i,j} + subject(k).ERO{i,j};
+            subject(1).PLI{i,j} = subject(1).PLI{i,j} + subject(k).PLI{i,j};
+            subject(1).ERP{i,j} = subject(1).ERP{i,j} + subject(k).ERP{i,j};
+        end
+        subject(1).ERO{i,j} = subject(1).ERO{i,j} / n_sbj;
+        subject(1).PLI{i,j} = subject(1).PLI{i,j} / n_sbj;
+        subject(1).ERP{i,j} = subject(1).ERP{i,j} / n_sbj;
+    end            
+end
+
+mean_sbj = subject(1);
+mean_sbj.f_name = 'MEAN SUBJECT';
+mean_sbj.f_path = 'N/A';
+clear mean_sbj.raw_data
+    
+end
+
 %% Visuaslization
-
 function visualize_eros(subject)
-%% EROS
-figure
-subplot(2,2,1)
-contourf(subject.t,subject.f,subject.ERO{1,1},20,'LineStyle','none')
-xlabel('time [ms]')
-ylabel('frequency [Hz]')
-title(['ENERGY > CH1 - ' subject.trig_label{1}])
 
-subplot(2,2,2)
-contourf(subject.t,subject.f,subject.ERO{2,1},20,'LineStyle','none')
-xlabel('time [ms]')
-ylabel('frequency [Hz]')
-title(['ENERGY > CH2 - ' subject.trig_label{2}])
+for i = 1:length(subject)
+    files{i} = subject(i).f_name;
+end
+f_idx = listdlg('PromptString','Select files for visualization:','ListString',files);
 
-subplot(2,2,3)
-contourf(subject.t,subject.f,subject.PLI{1,1},20,'LineStyle','none')
-xlabel('time [ms]')
-ylabel('frequency [Hz]')
-title(['ENERGY > CH1 - '  subject.trig_label{1}])
-
-subplot(2,2,4)
-contourf(subject.t,subject.f,subject.PLI{2,1},20,'LineStyle','none')
-xlabel('time [ms]')
-ylabel('frequency [Hz]')
-title(['ENERGY > CH2 - '  subject.trig_label{2}])
+for i = 1:length(f_idx)
+    for j = 1:subject(f_idx(i)).n_ch
+        figure('name',subject(f_idx(i)).f_name)
+        subplot(2,3,1)
+        contourf(subject(i).t,subject(i).f,subject(i).ERO{1,j},20,'LineStyle','none')
+        xlabel('time [ms]')
+        ylabel('frequency [Hz]')
+        title(['ENERGY > ch: ' subject(i).labels{j} ' > evt: ' subject(i).trig_label{1}])
+    
+        subplot(2,3,4)
+        contourf(subject(i).t,subject(i).f,subject(i).ERO{2,j},20,'LineStyle','none')
+        xlabel('time [ms]')
+        ylabel('frequency [Hz]')
+        title(['ENERGY > ch: ' subject(i).labels{j} ' > evt: ' subject(i).trig_label{2}])
+        
+        subplot(2,3,2)
+        contourf(subject(i).t,subject(i).f,subject(i).PLI{1,j},20,'LineStyle','none')
+        xlabel('time [ms]')
+        ylabel('frequency [Hz]')
+        title(['PLI > ch: ' subject(i).labels{j} ' > evt: ' subject(i).trig_label{1}])
+        
+        subplot(2,3,5)
+        contourf(subject(i).t,subject(i).f,subject(i).PLI{2,j},20,'LineStyle','none')
+        xlabel('time [ms]')
+        ylabel('frequency [Hz]')
+        title(['PLI > ch: ' subject(i).labels{j} ' > evt: ' subject(i).trig_label{2}])
+        
+        subplot(2,3,3)
+        plot(subject(i).t,subject(i).ERP{1,j})
+        xlabel('time [ms]')
+        ylabel('voltage [uV]')
+        title(['ERP > ch: ' subject(i).labels{j} ' > evt: ' subject(i).trig_label{1}])
+        
+        subplot(2,3,6)
+        plot(subject(i).t,subject(i).ERP{2,j})
+        xlabel('time [ms]')
+        ylabel('voltage [uV]')
+        title(['ERP > ch: ' subject(i).labels{j} ' > evt: ' subject(i).trig_label{2}])
+    end                          
+end
 end
 
 
--- README.md
+++ README.md
@@ -5,10 +5,11 @@ ERO_HUMAN.m:
 
 Is a piece of code for human eeg. The script reads EDF+ files with triggers.
 
+-select multiple electrodes (use control+click or multiple selectin) - the script evaluates all
+-select two triggers (target/non-target) - the script operates on the fixed protocol
+-select files to visualize - the script give names to figures and subplots automatically
+-MEAN SUBJECT structure contains averages falues of ERP,PLI,ERP over all files
 
-EROS_ROI.m:
-
-Doesn't work for this version
 
 
 
