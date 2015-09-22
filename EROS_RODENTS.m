6c21c988121fd026ad7b07500fad0d2a03af10f4
VlastaKoudelka <vlasta.koudelka@gmail.com>
2015-09-22 18:02:05 +0200

    Computes mean spectral power and ERP spectral power

--- EROS_RODENTS.m
+++ EROS_RODENTS.m
@@ -8,7 +8,7 @@ function EROS_RODENTS
 %                 | (____/\| ) \ \__| (___) |/\____) |
 %                 (_______/|/   \__/(_______)\_______)
 %                                   
-%  modified> 22.8.2015                         coded by> Vlastimil Koudelka
+%  modified> 22.9.2015                         coded by> Vlastimil Koudelka
 %                                       used code by>Robert Glenn Stockwell
 % 
 % - for optimal performance set a number of parallel workers:
@@ -29,13 +29,17 @@ end
 [raw_data, flags, labels] = read_data(path,names);
 
 parfor i = 1:length(raw_data)   %over all channels
-    [NOT_T_ERO{i},T_ERO{i},T_ERP{i}, NOT_T_ERP{i}, T_PLI{i}, NOT_T_PLI{i}, f{i},t{i}] = EROS_CALC(raw_data{i},flags{i});
+    [NOT_T_ERO{i},T_ERO{i}, NOT_T_ERP_ERO{i}, T_ERP_ERO{i}, NOT_T_ERP{i}, T_ERP{i}, NOT_T_PLI{i}, T_PLI{i}, f{i},t{i}] = EROS_CALC(raw_data{i},flags{i});
 end
 
 MEAN_POW{1} = zeros(size(NOT_T_ERO{1}));  %CH1 target
 MEAN_POW{2} = zeros(size(NOT_T_ERO{1}));  %CH2 target
 MEAN_POW{3} = zeros(size(NOT_T_ERO{1}));  %CH1 not-target
 MEAN_POW{4} = zeros(size(NOT_T_ERO{1}));  %CH2 not_target
+MEAN_ERP_POW{1} = zeros(size(NOT_T_ERO{1}));  %CH1 target
+MEAN_ERP_POW{2} = zeros(size(NOT_T_ERO{1}));  %CH2 target
+MEAN_ERP_POW{3} = zeros(size(NOT_T_ERO{1}));  %CH1 not-target
+MEAN_ERP_POW{4} = zeros(size(NOT_T_ERO{1}));  %CH2 not_target
 MEAN_PLI{1} = zeros(size(NOT_T_ERO{1}));  %CH1 target
 MEAN_PLI{2} = zeros(size(NOT_T_ERO{1}));  %CH2 target
 MEAN_PLI{3} = zeros(size(NOT_T_ERO{1}));  %CH1 not-target
@@ -53,6 +57,11 @@ for i = 1:n_mice                    %average over all mice
     MEAN_POW{3} = MEAN_POW{3} + NOT_T_ERO{2*i - 1}/n_mice; %CH1 not-target
     MEAN_POW{4} = MEAN_POW{4} + NOT_T_ERO{2*i}/n_mice;     %CH2 target
     
+    MEAN_ERP_POW{1} = MEAN_ERP_POW{1} + T_ERP_ERO{2*i - 1}/n_mice;     %CH1 target
+    MEAN_ERP_POW{2} = MEAN_ERP_POW{2} + T_ERP_ERO{2*i}/n_mice;         %CH2 target
+    MEAN_ERP_POW{3} = MEAN_ERP_POW{3} + NOT_T_ERP_ERO{2*i - 1}/n_mice; %CH1 not-target
+    MEAN_ERP_POW{4} = MEAN_ERP_POW{4} + NOT_T_ERP_ERO{2*i}/n_mice;     %CH2 target
+    
     MEAN_PLI{1} = MEAN_PLI{1} + T_PLI{2*i - 1}/n_mice;     %CH1 target
     MEAN_PLI{2} = MEAN_PLI{2} + T_PLI{2*i}/n_mice;         %CH2 target
     MEAN_PLI{3} = MEAN_PLI{3} + NOT_T_PLI{2*i - 1}/n_mice; %CH1 not-target
@@ -66,12 +75,12 @@ end
 f = f{1};
 t = t{1};
 toc
-visualize_eros(MEAN_POW, MEAN_PLI, MEAN_ERP, f, t);
-save ROI_in MEAN_POW MEAN_PLI MEAN_ERP T_ERO NOT_T_ERO T_ERP NOT_T_ERP T_PLI NOT_T_PLI labels f t
+visualize_eros(MEAN_POW, MEAN_ERP_POW, MEAN_PLI, MEAN_ERP, f, t);
+save ROI_in MEAN_POW MEAN_ERP_POW MEAN_PLI MEAN_ERP T_ERO NOT_T_ERO T_ERP NOT_T_ERP T_PLI NOT_T_PLI labels f t
 end
 
 %% EROS calculation
-function [A_rel_pow, B_rel_pow,T_ERP, NOT_T_ERP, B_PLI, A_PLI, f, t] = EROS_CALC(data, flags)
+function [A_rel_ERO, B_rel_ERO, A_AVG_ERO, B_AVG_ERO, NOT_T_ERP, T_ERP, A_PLI, B_PLI, f, t] = EROS_CALC(data, flags)
 t_pre = 200*1e-3;            %start trial before trigger [s]
 t_post = 1000*1e-3;          %stop trial after trigger [s]
 delay = flags(1,3);          %some delay of trigger flag [s]
@@ -131,6 +140,11 @@ for i = 1:size(B,1)
     [B_ST{i},t,f] = st(B(i,:),0,f_max,T, f_res);
 end
 
+[A_AVG_ERO,t,f] = st(NOT_T_ERP,0,f_max,T, f_res);  %S-transformation
+[B_AVG_ERO,t,f] = st(T_ERP,0,f_max,T, f_res);      %for energy
+
+A_AVG_ERO = abs(A_AVG_ERO);
+B_AVG_ERO = abs(B_AVG_ERO);
 %% Postprocessing
 for i = 1:size(A_ST{1},1)           %mean value calculation
     for j = 1:size(A_ST{1},2)
@@ -140,7 +154,7 @@ for i = 1:size(A_ST{1},1)           %mean value calculation
             cum_abs   = cum_abs + abs(A_ST{k}(i,j));
             cum_phase = cum_phase + A_ST{k}(i,j)/abs(A_ST{k}(i,j));
         end
-        A_mean(i,j) = cum_abs/length(A_ST);         %mean absolute value
+        A_ERO(i,j) = cum_abs/length(A_ST);         %mean absolute value
         A_PLI(i,j)  = abs(cum_phase/length(A_ST));  %PLI
         cum_abs = 0;
         cum_phase = 0;
@@ -148,13 +162,16 @@ for i = 1:size(A_ST{1},1)           %mean value calculation
             cum_abs   = cum_abs + abs(B_ST{k}(i,j));
             cum_phase = cum_phase + B_ST{k}(i,j)/abs(B_ST{k}(i,j));
         end
-        B_mean(i,j) = cum_abs/length(B_ST);
+        B_ERO(i,j) = cum_abs/length(B_ST);
         B_PLI(i,j)  = abs(cum_phase/length(B_ST)); 
     end
 end
 
-A_rel_pow = A_mean.^2/max(max(A_mean.^2));    %Relative spectral pow.
-B_rel_pow = B_mean.^2/max(max(B_mean.^2));
+A_rel_ERO = A_ERO.^2/max(max(A_ERO.^2));    %Relative spectral pow.
+B_rel_ERO = B_ERO.^2/max(max(B_ERO.^2));
+
+A_rel_AVG_ERO = A_AVG_ERO.^2/max(max(A_AVG_ERO.^2));    %Relative spectral pow. Averaged
+B_rel_AVG_ERO = B_AVG_ERO.^2/max(max(B_AVG_ERO.^2));
 
 t = (t - t_pre)*1e3;       %Time axis [ms]
 
@@ -255,7 +272,7 @@ end
 
 %% Visualization
 
-function visualize_eros(ERO_vis, PLI_vis, ERP_vis, f, t)
+function visualize_eros(ERO_vis, AVG_ERO_vis, PLI_vis, ERP_vis, f, t)
 %% EROS
 figure
 subplot(2,2,1)
@@ -281,6 +298,32 @@ contourf(t,f,ERO_vis{4},20,'LineStyle','none')
 xlabel('time [ms]')
 ylabel('frequency [Hz]')
 title('ENERGY > CH2 - non-target')
+
+%% EROS AVERAGED (based on ERP)
+figure
+subplot(2,2,1)
+contourf(t,f,AVG_ERO_vis{1},20,'LineStyle','none')
+xlabel('time [ms]')
+ylabel('frequency [Hz]')
+title('ERP ENERGY > CH1 - target')
+
+subplot(2,2,2)
+contourf(t,f,AVG_ERO_vis{2},20,'LineStyle','none')
+xlabel('time [ms]')
+ylabel('frequency [Hz]')
+title('ERP ENERGY > CH2 - target')
+
+subplot(2,2,3)
+contourf(t,f,AVG_ERO_vis{3},20,'LineStyle','none')
+xlabel('time [ms]')
+ylabel('frequency [Hz]')
+title('ERP ENERGY > CH1 - non-target')
+
+subplot(2,2,4)
+contourf(t,f,AVG_ERO_vis{4},20,'LineStyle','none')
+xlabel('time [ms]')
+ylabel('frequency [Hz]')
+title('ERP ENERGY > CH2 - non-target')
 
 %% PLI
 figure
